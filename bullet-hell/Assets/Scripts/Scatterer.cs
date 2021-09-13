79c02748cabe1dff63599bb6bb617e788b374931
Ted Barrett <tlbarrett@student.unimelb.edu.au>
2021-09-13 22:05:22 +1000

    implemented scattering but without check for spawning over other objects

--- bullet-hell/Assets/Scripts/Scatterer.cs
+++ bullet-hell/Assets/Scripts/Scatterer.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Scatterer : MonoBehaviour
 {
-    [SerializeField] private GameObject[] objects;
+    [SerializeField] private GameObject[] sourceObjects;
 
     [SerializeField] private int[] weights;
 
@@ -13,8 +15,54 @@ public class Scatterer : MonoBehaviour
 
     [SerializeField] private int numObjects;
 
+    private enum RotationType
+    {
+        None = 0,
+        Square = 1,
+        All = 2
+    }
+
+    [SerializeField] private RotationType rotationType = RotationType.None;
+
     private void Start()
     {
-        var clone = Instantiate(objects[0],new Vector3(0,0,0),Quaternion.identity);
+        Vector3 planePosOriginal = spawnPlane.transform.position;
+        Quaternion planeRotOriginal = spawnPlane.transform.rotation;
+
+        spawnPlane.transform.position = new Vector3(0, 0, 0);
+        spawnPlane.transform.rotation = Quaternion.identity;
+        
+        int totalWeight = weights.Sum();
+        int cumWeight;
+        int rndWeight = Random.Range(1, totalWeight);
+
+        GameObject[] spawnedObjects = new GameObject[numObjects];
+
+        for (int i = 0; i < numObjects; i++)
+        {
+            cumWeight = 0;
+            for (int j = 0; j < weights.Length; j++)
+            {
+                cumWeight += weights[j];
+                if (cumWeight >= rndWeight)
+                {
+                    spawnedObjects[i] = Instantiate(sourceObjects[j],
+                        new Vector3(Random.Range(-2f, 2f), 0, Random.Range(-2f, 2f)),
+                        Quaternion.identity);
+                    spawnedObjects[i].transform.parent = spawnPlane.transform;
+                    
+                    if (rotationType == RotationType.Square)
+                    {
+                        spawnedObjects[i].transform.rotation=Quaternion.Euler(0,Random.Range(0,3)*90f,0);
+                    } else if (rotationType == RotationType.All)
+                    {
+                        spawnedObjects[i].transform.rotation=Quaternion.Euler(0,Random.value*360f,0);
+                    }
+                }
+            }
+        }
+
+        spawnPlane.transform.position = planePosOriginal;
+        spawnPlane.transform.rotation = planeRotOriginal;
     }
 }
