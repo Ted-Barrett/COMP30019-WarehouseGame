1d273957241bc75eaea46713ecd66d70b0032c2b
Ted Barrett <tlbarrett@student.unimelb.edu.au>
2021-09-20 17:40:12 +1000

    modified player speed normalization and added gravity, jumping

--- bullet-hell/Assets/Scripts/PlayerScript.cs
+++ bullet-hell/Assets/Scripts/PlayerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
@@ -8,13 +9,30 @@ public class PlayerScript : MonoBehaviour {
 
     private CharacterController controller;
 
+    private float vSpeed = 0.0f;
+
+    [SerializeField] private float jumpSpeed = 4;
+    
     void Start() {
         controller = gameObject.GetComponent<CharacterController>();
     }
 
     void Update() {
         // We use GetAxisRaw here, becuase GetAxis returns a smoothed value that goes between no key press and full key press kind of slowly.
-        Vector3 inputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        Vector3 inputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+        if (inputMovement.sqrMagnitude > 1.0)
+        {
+            inputMovement.Normalize();
+        }
+        
+        vSpeed -= Physics.gravity.magnitude*Time.deltaTime;
+        
+        if (controller.isGrounded)
+        {
+            vSpeed = Input.GetAxisRaw("Jump") * jumpSpeed; // if we want to disable jumping, do vSpeed = 0; instead.
+        }
+
         controller.Move(inputMovement * Time.deltaTime * speed);
+        controller.Move(new Vector3(0, vSpeed*Time.deltaTime, 0));
     }
 }
