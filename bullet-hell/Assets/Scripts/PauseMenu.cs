58452052e8d229025aeacbddd7b6ffecd6780b39
crispycaterpillar <37645482+crispycaterpillar@users.noreply.github.com>
2021-10-28 23:19:53 +1100

    added volume settings, only in pause menu so far

--- bullet-hell/Assets/Scripts/PauseMenu.cs
+++ bullet-hell/Assets/Scripts/PauseMenu.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour {
-    
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    private GameObject audioSettings;
 
     void Start() {
+        audioSettings = transform.Find("AudioSettings").gameObject;
         Resume();
     }
     
@@ -23,6 +25,7 @@ public class PauseMenu : MonoBehaviour {
 
     public void Resume() {
         pauseMenuUI.SetActive(false);
+        audioSettings.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
@@ -39,5 +42,23 @@ public class PauseMenu : MonoBehaviour {
 
     public void QuitGame() {
         SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void AudioSettings()
+    {
+        pauseMenuUI.SetActive(false);
+        audioSettings.SetActive(true);
+        Slider[] sliders = audioSettings.GetComponentsInChildren<Slider>();
+        
+        foreach(Slider slider in sliders)
+        {
+            slider.gameObject.GetComponent<SliderScript>().SetSliderValue();
+        }
+    }
+
+    public void BackAudioSettings()
+    {
+        audioSettings.SetActive(false);
+        pauseMenuUI.SetActive(true);
     }
 }
---
+++ bullet-hell/Assets/Scripts/SliderScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum PlayerPrefsKeys
+{
+    MusicVolume,
+    SFXVolume
+};
+
+public class SliderScript : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerPrefsKeys sliderProperty;
+
+    public void SetSliderValue()
+    {
+        GetComponent<Slider>().value = PlayerPrefs.GetFloat(sliderProperty.ToString());
+    }
+
+    public void OnSliderChange()
+    {
+        PlayerPrefs.SetFloat(sliderProperty.ToString(), GetComponent<Slider>().value);
+    }
+}
